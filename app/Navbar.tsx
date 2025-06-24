3bfec81bca704bae0ee75874c2ec593050f5458c
chrisalex30 <akshara155301@gmail.com>
2025-06-24 10:47:04 +0530

    chore:Navbar Completed

---
+++ app/Navbar.tsx
@@ -0,0 +1,43 @@
+'use client'
+import Link from "next/link"
+import { usePathname } from "next/navigation"
+import {AiFillBug} from 'react-icons/ai'
+import classname from 'classnames'
+
+const Navbar = () => {
+
+    const currentPath=usePathname()
+    console.log(currentPath)
+
+    const links=[
+        {label:'Dashboard',href:'/'},
+        {label:'Issues',href:'/issues'}
+    ]
+
+  return (
+    <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
+        <Link href='/'><AiFillBug/></Link>
+        <ul className="flex space-x-6">
+            {
+                links.map((link)=>
+                    <li key={link.href}>
+                {/* <Link className={`${currentPath===link.href?"text-zinc-900":"text-zinc-500"} hover:text-zinc-800 transition-colors`}
+                 href={link.href}>{link.label}</Link> */}
+                 <Link href={link.href}
+                 className={
+                    classname({
+                        'text-zinc-900':currentPath===link.href,
+                        'text-zinc-500':currentPath!==link.href,
+                        'hover:text-zinc-800 transition-colors':true
+                    })
+                 }
+                 >{link.label}</Link>
+                 </li>    
+                )
+            }
+        </ul>
+    </nav>
+  )
+}
+
+export default Navbar
