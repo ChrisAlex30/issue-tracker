8a68c20aeda2944ddc0cca6361cd99a1159e4885
chrisalex30 <akshara155301@gmail.com>
2025-12-01 12:49:06 +0530

    chore: charts ui on dashboard done

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,34 @@
 import Image from "next/image";
+import LatestIssues from "./LatestIssues";
+import IssueSummary from "./IssueSummary";
+import IssueChart from "./IssueChart";
+import { prisma } from "@/lib/prisma";
+import { Flex, Grid } from "@radix-ui/themes";
 
-export default function Home() {
+export default async function Home() {
+  const open=await prisma.issue.count({
+    where:{
+        status:"OPEN"
+    }
+  })
+  const inProgress=await prisma.issue.count({
+    where:{
+        status:"IN_PROGRESS"
+    }
+  })
+  const closed=await prisma.issue.count({
+    where:{
+        status:"CLOSED"
+    }
+  })
   return (
+   <Grid columns={{initial:"1" , md:"2"}} gap="5">
+    <Flex direction="column" gap="5">
+          <IssueSummary {...{open,closed,inProgress}}/>
+          <IssueChart {...{open,closed,inProgress}}/>
+    </Flex>
+          <LatestIssues/>
+   </Grid>
    
-   <div>
-    Hello World
-   </div>
   );
 }
